23e201359ed1c98801a3349d3061668ef029c633
COULIBALY Bourahima <91073443+CouLiBaLy-B@users.noreply.github.com>
2023-02-08 21:10:49 +0100

    Add files via upload

---
+++ src/Bellman_FordRcpp.cpp
@@ -0,0 +1,44 @@
+#include <RcppArmadillo.h>
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+// [[Rcpp::export]]
+Rcpp::List bellmanFordRcpp(Rcpp::NumericMatrix adjacencyMatrix, int source){
+
+  int n = adjacencyMatrix.nrow();
+
+  Rcpp::NumericVector distance(n);
+  Rcpp::IntegerVector predecessor(n);
+
+  for (int i = 0; i < n; i++) {
+    distance[i] = R_PosInf;
+    predecessor[i] = -1;
+  }
+
+  distance[source] = 0;
+
+
+  for (int i = 1; i <= n-1; i++) {
+    for (int u = 0; u < n; u++) {
+      for (int v = 0; v < n; v++) {
+
+        if (adjacencyMatrix(u,v) > 0) {
+
+          double alt = distance[u] + adjacencyMatrix(u,v);
+
+          if (alt < distance[v]) {
+            distance[v] = alt;
+            predecessor[v] = u;
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+  return Rcpp::List::create(_["distance"]=distance, _["predecessor"]=predecessor +1 );
+}
